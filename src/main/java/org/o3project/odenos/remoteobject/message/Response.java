8b8bf2bae584615d3e52f70fc617cd9f83f4c29a
HIGUCHI Yuta <y-higuchi@ak.jp.nec.com>
2015-04-14 16:08:22 -0700

    Change status code constants to primitive type, so that it can be compared using ==, !=

--- src/main/java/org/o3project/odenos/remoteobject/message/Response.java
+++ src/main/java/org/o3project/odenos/remoteobject/message/Response.java
@@ -28,16 +28,16 @@ import java.io.IOException;
  */
 public class Response extends MessageBodyUnpacker {
 
-  public static final Integer OK = 200;
-  public static final Integer CREATED = 201;
-  public static final Integer ACCEPTED = 202;
-  public static final Integer NO_CONTENT = 204;
-  public static final Integer BAD_REQUEST = 400;
-  public static final Integer FORBIDDEN = 403;
-  public static final Integer NOT_FOUND = 404;
-  public static final Integer METHOD_NOT_ALLOWED = 405;
-  public static final Integer CONFLICT = 409;
-  public static final Integer INTERNAL_SERVER_ERROR = 500;
+  public static final int OK = 200;
+  public static final int CREATED = 201;
+  public static final int ACCEPTED = 202;
+  public static final int NO_CONTENT = 204;
+  public static final int BAD_REQUEST = 400;
+  public static final int FORBIDDEN = 403;
+  public static final int NOT_FOUND = 404;
+  public static final int METHOD_NOT_ALLOWED = 405;
+  public static final int CONFLICT = 409;
+  public static final int INTERNAL_SERVER_ERROR = 500;
 
   private static final int MSG_NUM = 2;
 
